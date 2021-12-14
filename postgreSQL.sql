94c56800791bd4bd40725e760fed54343d027432
rafaelccorrea <rafaelgustavocorrea@gmail.com>
2021-12-14 15:00:59 -0300

    GraphQl Atualizacao

--- postgreSQL.sql
+++ postgreSQL.sql
@@ -15,7 +15,7 @@ CREATE TABLE posts(
 	FOREIGN KEY(autorId) REFERENCES users(id)
 );
 
-INSERT INTO public.users (nome, email, createdat)
+INSERT INTO public.users (nome, email, createdAt)
 VALUES('Amanda Costa', 'a@a.com', CURRENT_TIMESTAMP),
 ('Beatriz Martins', 'b@b.com', CURRENT_TIMESTAMP),('Carla Souza', 'c@c.com',CURRENT_TIMESTAMP)
 
@@ -25,3 +25,7 @@ VALUES
    ('Iniciando em JS', 'Conteúdo para iniciantes', 1 , true),
    ('Dicas de React', 'Melhores práticas', 2 , true),
    ('Sintaxe CommonJS', 'Usando módulos',3 , false);
+
+ALTER TABLE "reviews" ADD FOREIGN KEY ("postId") REFERENCES "posts"("id") ON DELETE CASCADE ON UPDATE CASCADE
+
+ALTER TABLE "reviews" ADD FOREIGN KEY ("reviewerId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE
