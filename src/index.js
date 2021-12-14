426ad8996319fc6b2ed146e759a6b8611edcafbc
rafaelccorrea <rafaelgustavocorrea@gmail.com>
2021-12-14 15:36:43 -0300

    Nexus e Prisma

--- src/index.js
+++ src/index.js
@@ -1,69 +1,8 @@
-import { ApolloServer, gql } from 'apollo-server'
+import { ApolloServer } from 'apollo-server'
 import { PrismaClient } from '@prisma/client'
+import schema from './schema'
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient({ log: ['query']})
 
-const typeDefs = gql`
-  scalar DateTime
-  type User {
-    id: Int
-    nome: String
-    email: String
-    createdAt: DateTime
-    posts: [Post]
-  }
-  type Post {
-    id: Int
-    titulo: String
-    conteudo: String
-  }
-  type Query {
-    users: [User]
-    postsByUser (id: Int): [Post]
-    postsByReviewer (id: Int): [Post]
-  }
-  type Mutation {
-    createUserAndPost (nome: String, email: String, titulo: String, conteudo: String): User
-  }
-`
-
-const resolvers = {
- Query: {
-   users: async () => await prisma.user.findMany({
-     include: {
-       posts: true
-     }
-   }),
-   postsByUser: async (_, args) => {
-     return prisma.user
-      .findUnique({ where: { id: Number(args.id) }})
-      .posts()
-   },
-   postsByReviewer: async (_, args) => {
-     return prisma.review
-      .findUnique({ where: { id: Number(args.id) } })
-      .reviewer()
-      .posts()
-   },
- },
- Mutation: {
-   createUserAndPost: async (_, args) => {
-     const newUser = await prisma.user.create({
-       data: {
-         nome: args.nome,
-         email: args.email,
-         posts: {
-           create: {
-             titulo: args.titulo,
-             conteudo: args.conteudo
-           }
-         }
-       }
-     })
-     return newUser
-   }
- }
-}
-
-const server = new ApolloServer({ typeDefs, resolvers, context: prisma })
+const server = new ApolloServer({ schema, context: { prisma } })
 server.listen({ port: 3800 }, () => console.log(`Servidor pronto em localhost:3800`)) 
